89cee61f8f42ac6d886e0497bfbd5a237da87616
devopstrainers1 <47707015+devopstrainers1@users.noreply.github.com>
2021-04-24 21:12:24 +0530

    Delete demo.java

--- demo.java
+++
@@ -1,9 +0,0 @@
-This is demo java code
-DevOps batch
-This new line code
-This file is edited from wind
-Adding one more line
-GitHubDesktop
-This is added from MS VS CODE
-This Demo line
-This is to demo the git diff
